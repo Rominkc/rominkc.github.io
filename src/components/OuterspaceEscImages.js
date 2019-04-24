9196427bb023562169525ad685a7fbde82b5605a
Romin K <37375864+Rominkc@users.noreply.github.com>
2019-04-24 05:45:51 -0400

    Edit Resume

--- src/components/OuterspaceEscImages.js
+++ src/components/OuterspaceEscImages.js
@@ -2,7 +2,7 @@ import image1 from '../assets/Images/OuterspaceEsc1.jpg';
 import image2 from '../assets/Images/OuterspaceEsc2.jpg';
 import image3 from '../assets/Images/OuterspaceEsc3.jpg';
 import image4 from '../assets/Images/OuterspaceEsc4.jpg';
-import image5 from '../assets/Images/OuterspaceEsc5.jpg'
+import image5 from '../assets/Images/OuterspaceEsc5.jpg';
 /*Random Password Images Array to be used as sources  */
 export default [
 image1,
