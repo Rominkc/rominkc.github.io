4d7341b223a112ed4394a6d38ed6b430041a9b4d
Romin K <37375864+Rominkc@users.noreply.github.com>
2019-06-04 12:35:46 -0400

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-Personal Portfolio Website built with React
-Link to portfolio can be found here : https://rominkc.github.io/
--- src/components/RanPassImages.js
+++
@@ -1,9 +0,0 @@
-import image1 from '../assets/Images/RanPG1.jpg';
-import image2 from '../assets/Images/RanPG2.jpg';
-import image3 from '../assets/Images/RanPG3.jpg';
-/*Random Password Images Array to be used as sources  */
-export default [
-image1,
-image2,
-image3
-];
