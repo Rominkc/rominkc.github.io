67baf134042cee5ff21a1155718fdf289c6cc4af
Romin K <37375864+Rominkc@users.noreply.github.com>
2019-06-04 11:31:28 -0400

    First commit of reupload of portfolio website

---
+++ build/precache-manifest.39bae3726b1daaf401c63dfe575ec477.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "d598e820d7053ace7311e7c1f442a92a",
+    "url": "/static/media/OuterspaceEsc5.d598e820.jpg"
+  },
+  {
+    "revision": "79e20ae5bbb07f252173",
+    "url": "/static/css/main.e04615a3.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2e4088b9525d457c24006091d36d514c",
+    "url": "/static/media/NycTraffic2.2e4088b9.mp4"
+  },
+  {
+    "revision": "5e0f909498ad1be58bfa",
+    "url": "/static/js/2.5e0f9094.chunk.js"
+  },
+  {
+    "revision": "a00eb0f32cc49872c9e694c3ed6fd70e",
+    "url": "/static/media/Romin_Kershaw_Resume_2019.a00eb0f3.pdf"
+  },
+  {
+    "revision": "c7625950ad0f86461b25f2a75a02d08a",
+    "url": "/static/media/RanPG1.c7625950.jpg"
+  },
+  {
+    "revision": "476dfcc714808a93e6d901101e4ced1f",
+    "url": "/static/media/RanPG2.476dfcc7.jpg"
+  },
+  {
+    "revision": "1e0eb9b441c4d48d2b53696bb3aa442c",
+    "url": "/static/media/RanPG3.1e0eb9b4.jpg"
+  },
+  {
+    "revision": "2e7f850dce2a6d7c11797b385f2611e2",
+    "url": "/static/media/OuterspaceEsc1.2e7f850d.jpg"
+  },
+  {
+    "revision": "c1aa5166775b490d377b72d126d6c4bc",
+    "url": "/static/media/OuterspaceEsc2.c1aa5166.jpg"
+  },
+  {
+    "revision": "bc1d38e95589f005dd088bbb0799f9d2",
+    "url": "/static/media/OuterspaceEsc3.bc1d38e9.jpg"
+  },
+  {
+    "revision": "9907975e0ded9144ae47184551abf0f4",
+    "url": "/static/media/OuterspaceEsc4.9907975e.jpg"
+  },
+  {
+    "revision": "79e20ae5bbb07f252173",
+    "url": "/static/js/main.79e20ae5.chunk.js"
+  },
+  {
+    "revision": "46ab9b57e42194fdc7c83ec392d8c9cf",
+    "url": "/static/media/BlockSlice1.46ab9b57.jpg"
+  },
+  {
+    "revision": "aa8aced91e2fedac5b90f5efb16fa616",
+    "url": "/static/media/BlockSlice2.aa8aced9.jpg"
+  },
+  {
+    "revision": "25b265227c3885ce7b35b37f96bd45ea",
+    "url": "/static/media/BlockSlice3.25b26522.jpg"
+  },
+  {
+    "revision": "fdeaf032fb941232919293a5d1a3fbad",
+    "url": "/static/media/BlockSlice4.fdeaf032.jpg"
+  },
+  {
+    "revision": "e0e571bc12d459837301a15982801747",
+    "url": "/static/media/BlockSlice5.e0e571bc.jpg"
+  },
+  {
+    "revision": "b7907892c64b5114a53c930fdeab33d1",
+    "url": "/static/media/BlockSlice6.b7907892.jpg"
+  },
+  {
+    "revision": "a0d8cb8cacdd1ed902c49d61586573cc",
+    "url": "/static/media/UpNxtCrypto1.a0d8cb8c.jpg"
+  },
+  {
+    "revision": "1c4cbdaca9049a95fd3e798a273d966c",
+    "url": "/static/media/UpNxtCrypto2.1c4cbdac.jpg"
+  },
+  {
+    "revision": "b01457e5f54cf3dd16b7722ab176ec22",
+    "url": "/static/media/UpNxtCrypto3.b01457e5.jpg"
+  },
+  {
+    "revision": "3177b17a567826aee6b8214aa51595a7",
+    "url": "/static/media/UpNxtCrypto4.3177b17a.jpg"
+  },
+  {
+    "revision": "51e00467886d4de5b9086a49a17ae976",
+    "url": "/static/media/UpNxtCrypto5.51e00467.jpg"
+  },
+  {
+    "revision": "5e0f909498ad1be58bfa",
+    "url": "/static/css/2.b40666b6.chunk.css"
+  },
+  {
+    "revision": "8ef6e860a364237ccee4c0e2646a7dec",
+    "url": "/index.html"
+  }
+];
