dd78b3586817c4cbd24df9f14dc7326a5b9a4a46
Romin K <37375864+Rominkc@users.noreply.github.com>
2019-06-04 10:58:42 -0400

    Dummy change to try to force update

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
+//about page
 import './About.css';
 const About = (props) => {
 
